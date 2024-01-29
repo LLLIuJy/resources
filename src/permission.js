f741ec7951a657239105480f425b756769eb6374
LLLIuJy <jiyeliu@aliyun.com>
2024-01-29 15:26:16 +0800

    调用action的位置

--- src/permission.js
+++ src/permission.js
@@ -20,6 +20,10 @@ router.beforeEach(async(to, from, next) => {
       // next（地址）并没有执行后置守卫
       nprogress.done()
     } else {
+      // 判断是否获取过资料
+      if(!store.getters.userId){
+        await store.dispatch('user/getUserInfo')
+      }
        next() // 放行
     }
   } else {
