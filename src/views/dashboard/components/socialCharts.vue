9b0be760ec359a33e82119843ca4ab13c378f0ed
LLLIuJy <jiyeliu@aliyun.com>
2024-02-07 22:38:03 +0800

    全部完成

---
+++ src/views/dashboard/components/socialCharts.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- 容器 -->
+  <div class="charts" ref="charts"></div>
+</template>
+
+<script>
+//引入echarts
+import echarts from "echarts";
+export default {
+  name: "",
+  mounted() {
+    //初始化echarts实例
+    let lineCharts = echarts.init(this.$refs.charts);
+    //配置数据
+    lineCharts.setOption({
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [358, 468, 326, 934, 456, 674, 652],
+          type: 'line',
+          areaStyle: {}
+        }
+      ],
+      //布局调试
+      grid: {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+      },
+      tooltip: {}
+    });
+  },
+};
+</script>
+
+<style scoped>
+.charts {
+  width: 100%;
+  height: 100%;
+}
+</style>
